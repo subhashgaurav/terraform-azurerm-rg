0a4f584db8921dad267a8a698532d1407926f0c8
subhashgaurav <91779017+subhashgaurav@users.noreply.github.com>
2023-05-13 14:54:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 ###################################
 
 
-resource "azurerm_resource_group","az-rg-1"{
-  name        = var.rg_name
-  location    = var.location
+resource "azurerm_resource_group" "az-rg-1" {
+  name     = var.rg_name
+  location = var.location
 }
